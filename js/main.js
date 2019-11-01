952d94ef5a7f03a5595bb01ae8e64ce32ed33458
Julio Benavente <32945829+julio-benavente@users.noreply.github.com>
2019-11-01 15:15:56 -0500

    UI 1v without footer

--- js/main.js
+++ js/main.js
@@ -3,7 +3,32 @@ console.log('hola')
 
 
 $(document).ready(function(){
-  $('.your-class').slick({
-
-  });
+	$('.your-class').slick({
+  infinite: false,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+	responsive: [
+	    {
+	      breakpoint: 1200,
+	      settings: {
+	        slidesToShow: 3,
+	        slidesToScroll: 3,
+	      }
+	    },
+	    {
+	      breakpoint: 1000,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 2
+	      }
+	    },
+	    {
+	      breakpoint: 600,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1
+	      }
+	    }
+	  ]
+});
 });
